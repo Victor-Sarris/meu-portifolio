966db025d2cc7de67633a41fd25f45946042f2e2
Victor Sarrís Silva Santos <devvictorsarris@gmail.com>
2025-08-04 12:48:34 -0300

    fazendo uns bagulho ai

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 function clicouBtn(){
     alert("Para entrar em contato comigo, basta enviar um email para: devvictorsarris@gmail.com")
 }
+
+const imagem = document.getElementById("img-pessoa");
+
+const imagens = [
+    "images/dev.png",
+    "images/dev2.jpg"
+];
+
+let indice = 0;
+
+setInterval(() => {
+  // Alterna entre 0 e 1
+  indice = (indice + 1) % imagens.length;
+  imagem.src = imagens[indice];
+}, 4000);
